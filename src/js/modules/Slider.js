558336de74a0b29cb46b1b89c2f4b83a15a15da8
Manuel Silva <manuel.silvag1@gmail.com>
2021-05-25 21:10:07 -0500

    Cambio de imágenes

--- src/js/modules/Slider.js
+++ src/js/modules/Slider.js
@@ -14,10 +14,11 @@ class Slider {
 
   static #createSliderItem(storie) {
     const sliderItem = document.createElement('div');
-    sliderItem.classList.add('customer-stories__item');
     const sliderMessage = document.createElement('p');
-    sliderMessage.classList.add('customer-stories__message');
     const sliderStorieAuthor = document.createElement('div');
+  
+    sliderItem.classList.add('customer-stories__item');
+    sliderMessage.classList.add('customer-stories__message');
     sliderStorieAuthor.classList.add('customer-stories__customer-name');
     
     sliderMessage.textContent = storie.message;
@@ -28,6 +29,45 @@ class Slider {
   
     return sliderItem;
   }
+
+  #createSliderControl(id) {
+    const sliderControl = document.createElement('div');
+    sliderControl.classList.add('customer-stories__control');
+    
+    sliderControl.addEventListener('click', () => {
+      this.#resetInterval();
+      this.moveSliderTo(id);
+      this.#cleanSliderElements();
+      this.#updateElementClasses();
+      this.#updateControlStyles();
+      this.#startInterval();
+    })
+    return sliderControl;
+  }
+
+  #startInterval() {
+    this.interval = setInterval(() => {
+      this.#cleanSliderElements();
+      this.moveSlider();
+      this.#updateElementClasses();
+      this.#updateControlStyles();
+    }, 5000)
+  }
+
+  #resetInterval() {
+    clearInterval(this.interval);
+  }
+
+  #updateControlStyles() {
+    this.sliderControls.forEach((control, index) => {
+      if (index === this.activeSlider) {
+        control.classList.add('customer-stories__control--active');
+      } else {
+        control.classList.remove('customer-stories__control--active');
+      }
+    })
+  }
+
 
   #cleanSliderElements() {
     Slider.#removeClasslist(this.sliderItems[this.activeSlider]); 
@@ -46,6 +86,12 @@ class Slider {
   
   initSlider() {
     this.sliderElement = document.getElementById('customerStoriesSlider');
+    this.sliderControl = document.getElementById('sliderControl');
+    this.sliderControls = this.stories.map((storie, index) => {
+      const sliderControl = this.#createSliderControl(index);
+      this.sliderControl.appendChild(sliderControl);
+      return sliderControl;
+    })
     this.sliderItems = this.stories.map((storie) => {
       const sliderItem = Slider.#createSliderItem(storie);
       this.sliderElement.appendChild(sliderItem);
@@ -53,11 +99,8 @@ class Slider {
     })
     this.#cleanSliderElements();
     this.#updateElementClasses();
-    setInterval(() => {
-      this.#cleanSliderElements();
-      this.moveSlider();
-      this.#updateElementClasses();
-    }, 5000)
+    this.#updateControlStyles();
+    this.#startInterval();
   }
 
   moveSlider() {
@@ -66,6 +109,15 @@ class Slider {
     this.nextSlider++;
     if (this.activeSlider >= this.sliderItems.length) this.activeSlider = 0;
     if (this.previousSlider >= this.sliderItems.length) this.previousSlider = 0;
+    if (this.nextSlider >= this.sliderItems.length) this.nextSlider = 0;
+  }
+
+  moveSliderTo(num) {
+    this.activeSlider = num;
+    this.previousSlider = num - 1;
+    this.nextSlider = num + 1;
+    if (this.activeSlider >= this.sliderItems.length) this.activeSlider = 0;
+    if (this.previousSlider < 0) this.previousSlider = 2;
     if (this.nextSlider >= this.sliderItems.length) this.nextSlider = 0;
   }
 }
